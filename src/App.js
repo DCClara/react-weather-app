6f09539f7c6132e0015ba0bf9d253077cc742166
DCClara <141446800+DCClara@users.noreply.github.com>
2023-11-29 11:54:41 -0500

    removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
+import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  return <div className="App">Live Weather</div>;
 }
-
-export default App;
