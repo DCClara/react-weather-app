0abede00403d245840fe7ecdbf064d3611eee6eb
DCClara <141446800+DCClara@users.noreply.github.com>
2023-11-29 12:26:02 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Live Weather
-      <footer>
-        <p>
-          Coded by Danielle Casa and open-sourced on{" "}
-          <a href="https://github.com/DCClara/react-weather-app" target="blank">
-            Github
-          </a>
-        </p>
-      </footer>
+      <div className="container">
+        <h1>Live Weather</h1>
+        <footer>
+          <p>
+            Coded by Danielle Casa and open-sourced on{" "}
+            <a
+              href="https://github.com/DCClara/react-weather-app"
+              target="blank"
+            >
+              Github
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Weather from "./Weather";
+import "bootstrap/dist/css/bootstrap.css";
 
-import "./Weather.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -11,8 +10,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <Weather />
-    
   </React.StrictMode>
 );
 
