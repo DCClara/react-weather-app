ac9da2e7fc8f3d31b79627ac72dc3c38d4fa1214
DCClara <141446800+DCClara@users.noreply.github.com>
2023-11-29 12:50:01 -0500

    removed previous code and started new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Live Weather</h1>
+        <Weather />
         <footer>
           <p>
             Coded by Danielle Casa and open-sourced on{" "}
