a1b2550eb3670c038231d953f0279a0e49dfa59e
DCClara <141446800+DCClara@users.noreply.github.com>
2023-12-31 20:07:00 -0500

    made app responsive

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1 className="text-center">Live Weather</h1>
-        <h6 className="text-center">
-          created with <i className="fa-brands fa-react"></i>React
-        </h6>
         <Weather defaultCity="Fort Lauderdale" />
         <footer>
           <p>
